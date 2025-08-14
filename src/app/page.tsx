7aaaa708b8f1cbf03440fa8267403da28cb033e2
Arthur Barros <arthurpbarros@hotmail.com>
2025-08-14 12:56:00 -0300

    formatação de data com função no utils

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,41 +3,16 @@ import { Header } from '@/components/Header';
 import { PostsList } from '@/components/PostsList';
 import { SpinLoader } from '@/components/SpinLoader';
 import React, { Suspense } from 'react';
-import { PostHeading } from '@/components/PostHeading';
-import { PostCoverImage } from '@/components/PostCoverImage';
+import { FeaturedPost } from '@/components/FeaturedPost';
 
 export default async function HomePage() {
   return (
     <Container>
       <Header />
 
-      <section className='grid grid-cols-1 gap-8 mb-16 sm:grid-cols-2'>
-        <PostCoverImage
-          linkProps={{ href: '#' }}
-          imageProps={{
-            width: '1200',
-            height: '720',
-            src: '/images/bryen_9.png',
-            alt: 'alt da imagem',
-            priority: true,
-          }}
-        />
-
-        <div className='flex flex-col gap-4 sm:justify-center'>
-          <time
-            className='text-slate-600 text-sm/tight block dark:text-slate-300'
-            dateTime='2025-08-14'
-          >
-            14/08/2025 10:00
-          </time>
-          <PostHeading as='h1' url='#'>
-            commodi possimus repellat dolore
-          </PostHeading>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet eos ipsa
-          debitis commodi possimus repellat dolore ea in iste. Minima esse
-          expedita maiores aut modi corporis omnis et at reiciendis?
-        </div>
-      </section>
+      <Suspense fallback={<SpinLoader />}>
+        <FeaturedPost />
+      </Suspense>
 
       <Suspense fallback={<SpinLoader />}>
         <PostsList />
--- src/components/PostsList/index.tsx
+++ src/components/PostsList/index.tsx
@@ -2,16 +2,21 @@ import React from 'react';
 import { postRepository } from '@/repositories/post';
 import { PostCoverImage } from '../PostCoverImage';
 import { PostHeading } from '../PostHeading';
+import { formatDatetime, formatRelativeDate } from '@/utils/format-datetime';
 
 export async function PostsList() {
   const posts = await postRepository.findAll();
   return (
     <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8'>
       {posts.map(post => {
+        const postLink = `/post/${post.slug}`;
+
         return (
           <div className='flex flex-col gap-4 group' key={post.id}>
             <PostCoverImage
-              linkProps={{ href: `/post/${post.slug}` }}
+              linkProps={{
+                href: postLink,
+              }}
               imageProps={{
                 width: '1200',
                 height: '720',
@@ -24,10 +29,11 @@ export async function PostsList() {
               <time
                 className='text-slate-600 text-sm/tight block dark:text-slate-300'
                 dateTime={post.createdAt}
+                title={formatRelativeDate(post.createdAt)}
               >
-                {post.createdAt}
+                {formatDatetime(post.createdAt)}
               </time>
-              <PostHeading as='h2' url='#'>
+              <PostHeading as='h2' url={postLink}>
                 {post.title}
               </PostHeading>
               {post.excerpt}
